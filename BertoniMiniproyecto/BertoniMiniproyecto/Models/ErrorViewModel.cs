ee02f02862948f312702831121563d22d20da98a
emt38 <ezequieltheboss64@gmail.com>
2021-03-26 20:36:55 -0400

    Initial Commit

---
+++ BertoniMiniproyecto/BertoniMiniproyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BertoniMiniproyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
